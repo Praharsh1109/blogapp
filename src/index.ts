b0a3d40f11cc9253186da5f1bd87214d146c86f1
Praharsh1109 <ppraharsh11@gmail.com>
2024-06-27 19:27:15 -0400

    added signup and signin

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,72 @@
-import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { Hono } from 'hono';
+import { sign } from 'hono/jwt'
 
-const app = new Hono()
+// Create the main Hono app
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string,
+		JWT_SECRET: string,
+	}
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+		});
+		const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+		return c.json({ jwt });
+	} catch(e) {
+		c.status(403);
+		return c.json({ error: "error while signing up" });
+	}
 })
 
-app.post('/api/v1/user/signup',(c)=>{
-  return c.text('signup Page')
-})
+app.post('/api/v1/signin', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
 
-app.post('/api/v1/user/signin',(c)=>{
-  return c.text('signin Page')
-})
+	const body = await c.req.json();
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email
+		}
+	});
 
-app.post('/api/v1/blog',(c)=>{
-  return c.text(' posting blog page')
-})
+	if (!user) {
+		c.status(403);
+		return c.json({ error: "user not found" });
+	}
 
-app.put('/api/v1/blog',(c)=>{
-  return c.text(' delete blog page')
-})
-
-app.get('/api/v1/blog:id',(c)=>{
-  const id = c.req.param('id')
-	console.log(id);
-	return c.text('get blog route')
-})
-
-app.get('/api/v1/blog/bulk',(c)=>{
-  return c.text('getting bulk blogs')
+	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+	return c.json({ jwt });
 })
 
 
+app.get("/api/v1/blog/:id", (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("get blog route");
+});
 
-export default app
+app.post("/api/v1/blog", (c) => {
+  return c.text("signin route");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("signin route");
+});
+
+export default app;
